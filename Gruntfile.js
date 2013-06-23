2ac7a306bdff0b527f9b339599b20926002a1c07
Anselm Hannemann <info@anselm-hannemann.com>
2013-06-23 15:44:09 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,139 @@
+module.exports = function (grunt) {
+
+	'use strict';
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: require('./package'),
+		meta: {
+			banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+			'<%= grunt.template.today("yyyy-mm-dd") %> */'
+		},
+
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'js/*.js'
+			],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+
+		// Build modernizr
+		modernizr: {
+			devFile: 'js/vendor/modernizr-2.6.2.js',
+			outputFile : 'dist/js/vendor/modernizr-for-<%= pkg.version %>.min.js',
+
+			extra: {
+				shiv: true,
+				mq: true
+			},
+
+			// Minify
+			uglify: true,
+
+			// Files
+			files: ['js/**/*.js', 'scss/**/*.scss']
+		},
+
+		sass: {
+			dev: {
+				options: {
+					unixNewlines: true,
+					style: 'expanded'
+				},
+				files: {
+					'css/main.css': 'scss/main.scss'
+				}
+			},
+			deploy: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'dist/css/main-<%= pkg.version %>.min.css': 'scss/main.scss'
+				}
+
+			}
+		},
+
+		clean: {
+			deploy: ['dist']
+		},
+
+		requirejs: {
+			compile: {
+				options: {
+					baseUrl: 'js/',
+					mainConfigFile: 'js/config.js',
+					include: ['Vendor/require'],
+					out: 'dist/js/main-<%= pkg.version %>.min.js'
+				}
+			}
+		},
+
+		copy: {
+			deploy: {
+				files: [{
+					src: ['js/**'],
+					dest: 'dist/'
+				}]
+			}
+		},
+
+		watch: {
+			scss: {
+				files: ['scss/**/*.scss'],
+				tasks: 'sass:dev',
+				options: {
+					livereload: true
+				}
+			},
+
+			js: {
+				files: [
+					'Gruntfile.js',
+					'js/*.js'
+				],
+				tasks: 'jshint',
+				options: {
+					livereload: true
+				}
+			}
+		},
+
+		// Server config
+		connect: {
+			server: {
+				options: {
+					port: 9001,
+					keepalive: true
+				}
+			}
+		}
+	});
+
+	// Load some stuff
+	grunt.loadNpmTasks('grunt-modernizr');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+	// A task for development
+	grunt.registerTask('dev', ['jshint', 'sass:dev']);
+
+	// A task for deployment
+	grunt.registerTask('deploy', ['jshint', 'clean', 'modernizr', 'sass:deploy', 'requirejs', 'copy']);
+
+	// Default task
+	grunt.registerTask('default', ['jshint', 'sass:dev', 'requirejs', 'copy']);
+
+	// Travis CI task
+	grunt.registerTask('travis', ['jshint']);
+
+};
